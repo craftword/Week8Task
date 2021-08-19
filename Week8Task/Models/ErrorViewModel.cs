d6665ac244bdc270497c03b106c211dea9a45edc
Abimbola <craftwordltd@gmail.com>
2021-08-19 11:34:52 +0100

    first commit

---
+++ Week8Task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Week8Task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
